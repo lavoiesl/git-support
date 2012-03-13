37fefb32ae55f13763b239e14fef49be88f89033
Sébastien Lavoie <github@lavoie.sl>
2012-03-13 18:24:05 -0400

    added hooks/checkout-env-branch

---
+++ hooks/checkout-env-branch/README.md
@@ -0,0 +1,28 @@
+# Checkout an environment for matching branch
+
+The purpose of this hook is when you have different versions of the repository checked out at several places and you want to update them, tracking a branch.
+
+## Example
+If you develop a website that has an environment **dev** and **stage**, your tree could look like this:
+
+ * /mnt/repositories/**mywebsite**.git
+ * /mnt/environments/**mywebsite**/**dev**
+ * /mnt/environments/**mywebsite**/**stage**
+
+## Configuration
+For all the branches that are managed by this hook, a configuration should exists so that branch.<branch>.update-dir points to a valid git repository
+
+## Pushing
+When a push is made to the bare repository, two hooks are fired
+
+### update hook
+The update hook is fired before the update is made and it checks if it’s a commit on a branch.
+
+If branch.<branch>.update-dir exists, this repository is checked if it has local modifications.
+
+If it has modifications, an automatic commit is made and the update is cancelled.
+
+### post-update hook
+The update hook is fired after the update was succesful.
+
+If branch.<branch>.update-dir exists, this repository does a git pull -Xtheirs.
---
+++ hooks/checkout-env-branch/create-environments.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+# Script location
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Hooks to be copied
+hooks="update post-update"
+
+# Configuration
+root="$PWD"
+project_root="$root/repositories"
+env_root="$root/environments"
+default_env="dev"
+other_envs="stage"
+environments="$default_env $other_envs"
+
+# Variables and sanity check
+project_name="$1"
+
+if [[ -z "$project_name" ]]; then
+  echo "Usage: $0 <project-name>" >&2
+  exit 1
+fi
+
+bare_dir="$project_root/$project_name.git"
+envs_dir="$env_root/$project_name"
+
+if [[ -d "$bare_dir" ]]; then
+  echo "Project $project_name already exists" >&2
+  exit 2
+fi
+
+# Create bare repository
+git init --bare "$bare_dir"
+
+# Create root commit and all branches
+tmpdir="$(mktemp -d -t create-repo)"
+
+git clone "$bare_dir" "$tmpdir"
+
+cd "$tmpdir"
+
+git checkout -b $default_env
+git commit --allow-empty -m 'Initial commit'
+
+for env in $other_envs; do
+  git checkout -b $env
+done
+
+git push origin $environments
+
+cd "$bare_dir"
+
+# Change HEAD to $default_env
+git symbolic-ref HEAD refs/heads/$default_env
+
+rm -Rf $tmpdir
+
+# Checkout environments
+
+for env in $environments; do
+  env_dir="$envs_dir/$env"
+  GIT_DIR="$bare_dir" git config branch.$env.update-dir "$env_dir"
+  git clone "$bare_dir" -b $env "$env_dir"
+done
+
+# Add hooks
+for hook in $hooks; do
+  cp -v "$DIR/$hook" "$bare_dir/hooks/"
+done
